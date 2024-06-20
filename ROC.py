36ed3f4d8e01374a53099cd1aed5606267e3bd31
Monjoy Saha <monjoybme@gmail.com>
2024-06-20 13:51:35 -0400

    Updated

--- ROC.py
+++ ROC.py
@@ -1,10 +1,12 @@
 """
-Python code to compute the confidence interval for the Area Under the Curve (AUC) of a Receiver Operating Characteristic (ROC) curve using bootstrapping
+Python code to compute ROC and CI
 """
 
 import numpy as np
-from sklearn.metrics import roc_auc_score
+import pandas as pd
+from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.utils import resample
+import matplotlib.pyplot as plt
 
 def bootstrap_auc(y_true, y_score, n_bootstrap=1000, confidence_level=0.95):
     aucs = []
@@ -26,8 +28,29 @@ def bootstrap_auc(y_true, y_score, n_bootstrap=1000, confidence_level=0.95):
     return lower_bound, upper_bound
 
 # Example usage:
-# Assuming y_true and y_score are your true labels and predicted scores
-# y_true = ...
-# y_score = ...
+# Read data from CSV
+data = pd.read_csv('path_to_your_file.csv')
+
+# Assuming the CSV has columns 'y_true' and 'y_score'
+y_true = data['y_true'].values
+y_score = data['y_score'].values
+
+# Calculate AUC confidence intervals
 lower_bound, upper_bound = bootstrap_auc(y_true, y_score)
 print("95% Confidence Interval for AUC:", (lower_bound, upper_bound))
+
+# Plot ROC curve
+fpr, tpr, _ = roc_curve(y_true, y_score)
+roc_auc = roc_auc_score(y_true, y_score)
+
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
